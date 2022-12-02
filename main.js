cf558d2d47437b35f2029f8180a46b44ce4e78d2
Aada <juaa22qv@student.ju.se>
2022-12-03 00:01:51 +0100

    Start to the structure for order process

---
+++ main.js
@@ -0,0 +1,5 @@
+const results = document.getElementById("results");
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  results.append(`${name}: ${value}`);
+  results.append(document.createElement("br"));
+});
