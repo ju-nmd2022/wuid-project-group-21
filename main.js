aa12a400add34a870b3e98171c09e78e502df1f6
Aada <juaa22qv@student.ju.se>
2022-12-05 16:32:02 +0100

    Removing items from cart

--- main.js
+++ main.js
@@ -4,9 +4,53 @@ new URLSearchParams(window.location.search).forEach((value, name) => {
   results.append(document.createElement("br"));
 });
 
+const removeEggs = document.getElementById("removeEggs");
+const removeBlueberries = document.getElementById("removeBlueberries");
+const removeSyrup = document.getElementById("removeSyrup");
+const removePowder = document.getElementById("removePowder");
+const removeSugar = document.getElementById("removeSugar");
+const removeMilk = document.getElementById("removeMilk");
+const removeFlour = document.getElementById("removeFlour");
+
+const eggPrduct = document.getElementById("eggs");
+const blueberriesPrduct = document.getElementById("blueberries");
+const syrupPrduct = document.getElementById("syrup");
+const powderPrduct = document.getElementById("powder");
+const sugarPrduct = document.getElementById("sugar");
+const milkPrduct = document.getElementById("milk");
+const flourPrduct = document.getElementById("flour");
+
 function removeItemEggs() {
-  const eggPrduct = document.getElementById("eggs");
   eggPrduct.style.display = "none";
 }
-const removeEggs = document.getElementById("removeEggs");
 removeEggs.addEventListener("click", removeItemEggs);
+
+function removeItemBlueberris() {
+  blueberriesPrduct.style.display = "none";
+}
+removeBlueberries.addEventListener("click", removeItemBlueberris);
+
+function removeItemSyrup() {
+  syrupPrduct.style.display = "none";
+}
+removeSyrup.addEventListener("click", removeItemSyrup);
+
+function removeItemPowder() {
+  powderPrduct.style.display = "none";
+}
+removePowder.addEventListener("click", removeItemPowder);
+
+function removeItemSugar() {
+  sugarPrduct.style.display = "none";
+}
+removeSugar.addEventListener("click", removeItemSugar);
+
+function removeItemMilk() {
+  milkPrduct.style.display = "none";
+}
+removeMilk.addEventListener("click", removeItemMilk);
+
+function removeItemFlour() {
+  flourPrduct.style.display = "none";
+}
+removeFlour.addEventListener("click", removeItemFlour);
