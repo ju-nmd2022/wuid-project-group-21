c3e1067e356eb0d780c277c0f377f1444e334c5a
Aada <juaa22qv@student.ju.se>
2022-12-05 15:38:14 +0100

    "Order Details" -page layout with survey and items

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ new URLSearchParams(window.location.search).forEach((value, name) => {
   results.append(`${name}: ${value}`);
   results.append(document.createElement("br"));
 });
+
+function removeItemEggs() {
+  const eggPrduct = document.getElementById("eggs");
+  eggPrduct.style.display = "none";
+}
+const removeEggs = document.getElementById("removeEggs");
+removeEggs.addEventListener("click", removeItemEggs);
