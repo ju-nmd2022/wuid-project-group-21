38bfc04cae127897f78bc92b9870f50476b6914d
Tilde <joti22eo@student.ju.se>
2022-12-08 18:22:41 +0100

    Small fixes to the detail page

--- detail.js
+++ detail.js
@@ -1,20 +1,23 @@
 // Shopping bag
 
+// Selects all as element
 let totalCount = 0;
 document.querySelectorAll(".switch").forEach(function (element) {
-  console.log(element.firstElementChild);
+  console.log(element.firstElementChild); //First element child = checkbox not the whole switch
   const checkbox = element.firstElementChild;
   checkbox.addEventListener("click", function () {
     if (checkbox.checked) {
+      //+1 or -1 item when checkbox is clicked
       totalCount = totalCount + 1;
     } else {
       totalCount = totalCount - 1;
     }
-    console.log(totalCount);
+    console.log(totalCount); //Hides initial shoppingbag when totalCount is running
     document.querySelectorAll(".shoppingbagbutton").forEach(function (button) {
       button.style.display = "none";
     });
     if (totalCount !== 0) {
+      //If checkboxes are on, totalCount is showing, else initial shoppingbag is showing
       document.querySelector(".shoppingbag" + totalCount).style.display =
         "block";
     } else {
